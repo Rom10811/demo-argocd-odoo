ce7d2d7ee5f9156019ef216401ede22e65bfccf8
DUCIEL Romain <romain@mokatourisme.fr>
2025-08-23 15:56:54 +0200

    Test image perso

--- scripts/create-client.sh
+++ scripts/create-client.sh
@@ -2,6 +2,9 @@
 # scripts/create-client.sh
 
 set -e
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_ROOT="$(dirname "$SCRIPT_DIR")"
+cd "$PROJECT_ROOT"
 
 CLIENT_NAME=$1
 DB_NAME=$2
